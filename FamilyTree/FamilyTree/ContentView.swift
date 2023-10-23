c405c9ad91d3a2cc6f3387ed3af2e4e1d325303a
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-10-23 11:51:54 +0200

    Restructure Flow

--- FamilyTree/FamilyTree/ContentView.swift
+++ FamilyTree/FamilyTree/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum TreeLayout {
+    static let lineHeight: CGFloat = 10
+    static let lineOffset: CGFloat = 20
     
+    static let mainColor = Color.blue
+    
+    static let nodeWidth = 160
+    static let nodeHeight = 60
+    static let nodeSize = CGSize(width: nodeWidth, height: nodeHeight)
+}
+
+struct ContentView: View {
     @State private var currentZoom = 0.0
     @State private var totalZoom = 1.0
+    @State private var rotationAngle = Angle.zero
+    
+    private let screen = UIScreen.main.bounds.size
+    
     var body: some View {
         ScrollView([.horizontal, .vertical]) {
-            VStack(spacing: 10) {
+            VStack(spacing: TreeLayout.lineHeight) {
                 TrialView()
+                    .frame(width: screen.width, height: screen.height)
+//                    .background(.green)
                     .scaleEffect(currentZoom + totalZoom)
+                    .rotationEffect(rotationAngle)
+                    .contentShape(Rectangle())
                     .gesture(
                         MagnifyGesture()
                             .onChanged { value in
@@ -27,7 +45,6 @@ struct ContentView: View {
                             }
                     )
             }
-            
         }
     }
 }
@@ -38,17 +55,27 @@ struct ContentView: View {
 
 
 struct TrialView: View {
+    var accentColor = TreeLayout.mainColor
+    var offset = TreeLayout.lineOffset
     var rootNode = trial()
     var body: some View {
         
-        VStack(spacing: 10) {
+        VStack(spacing: offset) {
+//            ZStack {
             intro(rootNode)
-            HStack(spacing: 15) {
-                ForEach(rootNode.children) { fdescendant in
-                    TrialView(rootNode: fdescendant)
-                    
+                .overlay {
+                    HStack(alignment: .top, spacing: 15) {
+                        ForEach(rootNode.children) { fdescendant in
+                            TrialView(rootNode: fdescendant)
+//                                .background([Color.purple, Color.red, Color.green, Color.pink, Color.brown, Color.orange].randomElement()!)
+                        }
+                    }
+                    .offset(y: 60 + offset)
+//                    .fixedSize(horizontal: true, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                 }
-            }
+//            .offset(y: 60 + offset)
+//            .fixedSize()
+//            .background(.green)
         }
     }
     
@@ -59,54 +86,112 @@ struct TrialView: View {
             .padding()
             .minimumScaleFactor(0.5)
             .lineLimit(1)
-            .frame(width: 160)
-            .border(.red)
+            .frame(width: 160, height: 60)
+            .border(accentColor)
+            .frame(maxWidth: .infinity)
             .overlay(alignment: .bottom) {
-                if !node.children.isEmpty {
-                    HStack {
-                        ForEach(0..<node.children.count, id: \.self) { i in
-                            Color.red.frame(width: 1, height: 10)
-                                .frame(maxWidth: .infinity)
-                                .offset(y: 10)
+                
+                if node.children.count == 1 {
+                    accentColor.frame(width: 1, height: offset)
+                        .frame(maxWidth: .infinity)
+                        .offset(y: offset)
+                    
+                } else if node.children.count > 1 {
+                    VStack(spacing: 0) {
+                        
+                        accentColor.frame(width: 1, height: TreeLayout.lineHeight)
+                        
+//                        GeometryReader { geo in
+                            accentColor
+//                                .fixedSize(horizontal: false, vertical: true)
+//                                .frame(width: geo.size.width - 164, height: 1)
+                                .frame(
+                                    width: (160 * CGFloat(node.children.count)) + (15 * CGFloat(node.children.count-1)) - 160,
+                                    height: 1
+                                )
+                                .background(.brown)
+//                                .frame(maxWidth: .infinity)
+//                        }
+//                        .frame(height: 1)
+                        HStack(spacing: 15) {
+                            ForEach(0..<node.children.count, id: \.self) { i in
+                                accentColor
+                                    .frame(width: 1, height: TreeLayout.lineHeight)
+//                                    .frame(maxWidth: 300)
+                                if i+1 != node.children.count {
+                                    Spacer()
+                                }
+                            }
                         }
+                        
                     }
+//                    .background(Color.teal)
+                    
+//                    .offset(x: -20)
+//                    .frame(maxWidth: .infinity)
+                    .offset(y: offset)
                 }
             }
     }
     
     typealias Node = FamilyNode
-    static func trial() -> Node<String> {
-        let musole = Node(value: "MUSOLE")
-        
-        let mukembanyi = Node(value: "MUKEMBANYI")
-        let coldBeverages = Node(value: "cold")
-        
-        let bahirwe = Node(value: "BAHIRWE")
-        
+    
+    static func otherMusoles() -> [FamilyNode<String>] {
+        [
+            "MWENGEHERWA", "NAMWEZI",
+            "MUKEMBANYI", "ZIRIMWABA",
+            "CIBALONZA", "CHARLOTTE", "CIREZI"
+        ].map(FamilyNode.init)
+    }
+    static func otherMukembanyis() -> [FamilyNode<String>] {
+        [
+            "BISIMA", "KAJIBWAMI", "KUJIRAKWINJA", "MARIO?", "BACIKENGE", "KULIMUSHI",
+            "BAHIRWE",
+            "NSIMIRE", "MAPENDO","SCOLASTIC", "VELENTINE", "NZIGIRE"
+            
+        ].map(FamilyNode.init)
+    }
+    
+    static func otherBahirwes() -> [FamilyNode<String>] {
         let ciri = Node(value: "CIRIMWAMI")
         let aga = Node(value: "AGANZE")
         let nabi = Node(value: "NABINTU")
         let bulo = Node(value: "BULONZA")
         let ira = Node(value: "IRAGI")
         
-               
-        musole.add(child: mukembanyi)
-        
-        mukembanyi.add(child: bahirwe)
-        
-        bahirwe.add(child: ciri)
-        bahirwe.add(child: aga)
-        bahirwe.add(child: nabi)
-        bahirwe.add(child: bulo)
-        bahirwe.add(child: ira)
-            
+        return [ciri, aga, nabi, bulo, ira]
+    }
     
-        print(musole)
-        return musole
+    static func otherKanywenges() -> [FamilyNode<String>] {
+        ["MWA BAHENE", "MUSOLE","MWA MUNANA"].map(FamilyNode.init)
+    }
+    
+    static func trial() -> Node<String> {
+        let birhonga = Node(value: "BIRHONGA")
+        
+        // 1st Generation
+        let kanywenge = Node(value: "KANYWENGE")
+        birhonga.add(child: kanywenge)
+        
+        // 2nd Generation
+        kanywenge.add(children: otherKanywenges())
+        
+        if let musole = kanywenge.search(value: "MUSOLE") {
+            // 3rd Generation
+            if let mukembanyi = musole.search(value: "MUKEMBANYI") {
+                // 4th Generation
+                mukembanyi.add(children: otherMukembanyis())
+                if let bahirwe = mukembanyi.search(value: "BAHIRWE") {
+                    // 5th Generation
+                    bahirwe.add(children: otherBahirwes())
+                }
+            }
+        }
+        
+        print(birhonga)
+        return birhonga
     }
 }
-
-
 
 
 class FamilyNode<T>: Identifiable {
@@ -121,6 +206,11 @@ class FamilyNode<T>: Identifiable {
     func add(child: FamilyNode) {
         children.append(child)
         child.parent = self
+    }
+    
+    func add(children elements: [FamilyNode]) {
+        children.append(contentsOf: elements)
+        elements.forEach { $0.parent = self }
     }
     
 }
